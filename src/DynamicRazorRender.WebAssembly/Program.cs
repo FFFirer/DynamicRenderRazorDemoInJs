8be36bb16663de8725431c801568ef8ea7f16ca1
fffirer <0sdf0@sina.cn>
2023-02-01 19:40:03 +0800

    优化发布

--- src/DynamicRazorRender.WebAssembly/Program.cs
+++ src/DynamicRazorRender.WebAssembly/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.RootComponents.RegisterCustomElement<DynamicRender>("render-razor");
+builder.RootComponents.RegisterAsCustomElement<DynamicRender>("render-razor");
 
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
